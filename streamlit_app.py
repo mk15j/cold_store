28f678712e1c1b668a89fbf281c4f59a6f36dd4d
mk15j <161435539+mk15j@users.noreply.github.com>
2024-03-22 11:23:40 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,13 @@ def generate_map(data):
 
     # Add markers and popups to the map
     for i, row in data.iterrows():
+        # Prepend '#' to the color value if not already present
+        color = '#' + row['color'].lower() if not row['color'].startswith('#') else row['color'].lower()
         folium.Marker(
             location=[row['lat'], row['lon']],
             popup=row['Description'],
             tooltip=f"Latitude: {row['lat']}, Longitude: {row['lon']}",
-            icon=folium.Icon(color=row['color'])
+            icon=folium.Icon(color=color)
         ).add_to(m)
 
     return m
@@ -30,12 +32,12 @@ if uploaded_file is not None:
         data = pd.read_excel(uploaded_file)
 
         # Check for required columns
-        required_columns = {'Name', 'Latitude', 'Longitude', 'Icon Color'}
+        required_columns = {'Name', 'Latitude', 'Longitude', 'Color'}
         if not required_columns.issubset(data.columns):
-            st.error("The uploaded file is missing one or more required columns: Name, Latitude, Longitude, Icon Color.")
+            st.error("The uploaded file is missing one or more required columns: Name, Latitude, Longitude, Color.")
         else:
             # Rename columns if necessary
-            data = data.rename(columns={'Name': 'name', 'Latitude': 'lat', 'Longitude': 'lon', 'Icon Color': 'color'})
+            data = data.rename(columns={'Name': 'name', 'Latitude': 'lat', 'Longitude': 'lon', 'Color': 'color'})
 
             # Convert lat and lon to numeric
             data['lat'] = pd.to_numeric(data['lat'], errors='coerce')
@@ -44,8 +46,18 @@ if uploaded_file is not None:
             # Drop rows with missing lat or lon values
             data = data.dropna(subset=['lat', 'lon'])
 
+            # Display DataFrame
+            st.write("### Data")
+            st.dataframe(data)
+
+            # Download link for DataFrame
+            csv_file = data.to_csv(index=False)
+            st.download_button(label="Download CSV", data=csv_file, file_name='Merchant_Exporters_ME.csv', mime='text/csv')
+
             # Display the map
+            st.write("### Map")
             map_data = generate_map(data)
             folium_static(map_data)
     except Exception as e:
         st.error(f"An error occurred: {e}")
+
